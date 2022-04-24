0e855d66dd3d30dfc6c3fe753f2ff8b15fab866a
WDSeo <wdseo@yonsei.ac.kr>
2022-04-25 03:06:53 +0900

    A + B - 3

---
+++ Class 1/Q10950.py
@@ -0,0 +1,8 @@
+import sys
+
+if __name__ == '__main__':
+    T = int(sys.stdin.readline().rstrip())
+
+    for _ in range(T):
+        A, B = map(int, sys.stdin.readline().rstrip().split())
+        print(A + B)
