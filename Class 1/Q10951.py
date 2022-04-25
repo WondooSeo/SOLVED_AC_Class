11570cc2348f7cd12e7c86d67669f892642a556d
WDSeo <wdseo@yonsei.ac.kr>
2022-04-26 00:34:46 +0900

    A + B - 4

--- Class 1/Q10951.py
+++ Class 1/Q10951.py
@@ -2,7 +2,7 @@ if __name__ == '__main__':
     while True:
         try:
             # You should use input() on try-except clause
-            # sys.stdin.readline().rstrip() will load one more \n which causes ValueError
+            # sys.stdin.readline().rstrip() will load last \n which causes ValueError
             # input() delete last \n
             A, B = map(int, input().split())
             print(A + B)
