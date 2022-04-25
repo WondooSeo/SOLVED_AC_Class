6d2fc951925f7129766ecf8cb133be0ba444ff19
WDSeo <wdseo@yonsei.ac.kr>
2022-04-26 00:33:43 +0900

    A + B - 4

---
+++ Class 1/Q10951.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    while True:
+        try:
+            # You should use input() on try-except clause
+            # sys.stdin.readline().rstrip() will load one more \n which causes ValueError
+            # input() delete last \n
+            A, B = map(int, input().split())
+            print(A + B)
+
+        except EOFError:
+            break
