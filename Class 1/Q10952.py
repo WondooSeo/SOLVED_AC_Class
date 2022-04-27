9ea555eb6edb88179793095bdd42953126e396d9
WDSeo <wdseo@yonsei.ac.kr>
2022-04-27 13:36:09 +0900

    A + B - 5

---
+++ Class 1/Q10952.py
@@ -0,0 +1,9 @@
+import sys
+
+if __name__ == '__main__':
+    while True:
+        A, B = map(int, sys.stdin.readline().rstrip().split())
+        if A == 0 and B == 0:
+            break
+
+        print(A + B)
