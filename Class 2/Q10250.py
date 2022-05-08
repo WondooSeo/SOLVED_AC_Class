a5fdf29e9683d426267b0fa6712d662635bfe3a0
WDSeo <wdseo@yonsei.ac.kr>
2022-05-08 20:47:53 +0900

    ACM 호텔

---
+++ Class 2/Q10250.py
@@ -0,0 +1,8 @@
+import sys
+from math import ceil
+
+if __name__ == '__main__':
+    T = int(sys.stdin.readline().rstrip())
+    for _ in range(T):
+        H, W, N = map(int, sys.stdin.readline().rstrip().split())
+        print(((N-1)%H + 1) * 100 + ((N-1)//H) + 1)
